9018bf052ab01497b89e25f341a38a2a8deb30df
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2021-12-23 14:50:01 -0300

    criando estrura para remoção de localidade

--- src/views/Notas/index.jsx
+++ src/views/Notas/index.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Card } from "../../components/Card";
 import { NavBar } from "../../components/NavBar";
 import { CardArea, Container, Content, Reviews } from "./styles";
 import { Review } from '../../components/Review';
-
+import { useNavigate } from 'react-router-dom';
 import localidade from '../../assets/images/house.jpg';
+import { Botao } from "../../components/Botao";
+import axios from "axios";
+import { baseURL } from "../../utils/api";
 
 export const Notas = () => {
     const { id } = useParams();
+
+    const [ loading, setLoading ] = useState(true);
+    const redirect = useNavigate();
+
+    const apagarLocalidade = async () => {
+        setLoading(true)
+        await axios({
+            method: 'DELETE',
+            url: `${baseURL}/localidades/${id}`
+        })
+        .then( response => {
+            alert('Localidade removida com sucesso')
+            redirect("/localidades")         
+        })
+        .catch(error => {console.log(error)})
+        .finally( () => setLoading(false));
+    }
     
     return(
         <Container>
@@ -16,8 +36,11 @@ export const Notas = () => {
             <Content>
                 <CardArea>
                     <Card img={localidade}/>
+                    <div onClick={() => apagarLocalidade()}>
+                        <Botao text="Apagar Localidade"/>
+                    </div>                    
                     <h1>Avaliações</h1>
-                </CardArea>
+                </CardArea>                
                 <Reviews>
                     <Review/>
                     <Review/>
